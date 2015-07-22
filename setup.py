ffa363e7d935eb973a79f8f9e3ad778b98155f42
Jean Jordaan <jean.jordaan@gmail.com>
2015-07-22 23:54:09 +0200

    Add some classifiers to setup.py

--- setup.py
+++ setup.py
@@ -7,13 +7,10 @@ text_based_switcher
 Text-based window switcher for Unity
 """
 
-# FIXME: Please read http://pythonhosted.org/setuptools/setuptools.html to
-#        customize in depth your setup script
-
 from setuptools import setup, find_packages
 import os, sys
 
-version = '1.0.0.dev0'
+version = '0.0.2.dev0'
 
 this_directory = os.path.abspath(os.path.dirname(__file__))
 
@@ -28,33 +25,38 @@ if sys.version_info < (2, 7):
     dev_require += ['unittest2']
 
 
-setup(name='text-based-switcher',
-      version=version,
-      description="Text-based window switcher for Unity",
-      long_description=long_description,
-      # FIXME: Add more classifiers from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
-          "Programming Language :: Python",
-          "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
-          ],
-      keywords='',  # FIXME: Add whatefer fits
-      author='Jean Jordaan',
-      author_email='jean.jordaan@gmail.com',
-      url='http://pypi.python.org/pypi/text-based-switcher',
-      license='GPLv3',
-      packages=find_packages('src'),
-      package_dir = {'': 'src'},
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          # 3rd party
-          'setuptools'
-          # Others
-          ],
-      entry_points={
-          },
-      tests_require=dev_require,
-      test_suite='tests.all_tests',
-      extras_require={
-          'dev': dev_require
-      })
+setup(
+    name='text-based-switcher',
+    version=version,
+    description="Text-based window switcher for Unity",
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python",
+        "Environment :: X11 Applications",
+        "Environment :: Plugins",
+        "Intended Audience :: End Users/Desktop",
+        "Topic :: Desktop Environment :: Window Managers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
+        ],
+    keywords='ubuntu unity switcher',
+    author='Jean Jordaan',
+    author_email='jean.jordaan@gmail.com',
+    url='http://pypi.python.org/pypi/text-based-switcher',
+    license='GPLv3',
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        # 3rd party
+        'setuptools'
+        # wmctrl or ruamel.ewmh
+        # Others
+        ],
+    entry_points={
+        },
+    tests_require=dev_require,
+    test_suite='tests.all_tests',
+    extras_require={
+        'dev': dev_require
+    })
